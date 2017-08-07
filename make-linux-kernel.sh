034a6dbe27dca49638cdfc743fbae3b205ab0b92
Sandesh Ghimire <sandeshghimire@gmail.com>
2017-08-07 12:26:59 -0700

    compile error

--- make-linux-kernel.sh
+++ make-linux-kernel.sh
@@ -2,9 +2,9 @@
 
 cd kernel
 #make defconfig distro.config
-make -j16
+make -j32
 make dtbs
-make savedefconfig 
+#make savedefconfig 
 cd ..
 rm -f -r dt.img
 ./skales/dtbTool -o dt.img -s 2048 kernel/arch/arm64/boot/dts/qcom/
