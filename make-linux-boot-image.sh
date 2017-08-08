f6f12a8a41d1537bda7ce02e4e36e7b991573db8
Sandesh Ghimire <sandeshghimire@gmail.com>
2017-08-08 11:30:16 -0700

    Fixed boot issue

--- make-linux-boot-image.sh
+++ make-linux-boot-image.sh
@@ -6,7 +6,7 @@
                    --dt dt.img \
                    --pagesize 2048 \
                    --base 0x80000000 \
-                   --cmdline "root=/dev/disk/by-partlabel/rootfs rw rootwait console=ttyMSM0,115200n8"
+                   --cmdline "root=/dev/mmcblk0p10 rw rootwait console=ttyMSM0,115200n8"
 
 
 
--- make-linux-kernel.sh
+++ make-linux-kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
 
 cd kernel
-#make defconfig distro.config
+make defconfig distro.config
 make -j32
 make dtbs
-#make savedefconfig 
+make savedefconfig 
 cd ..
 rm -f -r dt.img
 ./skales/dtbTool -o dt.img -s 2048 kernel/arch/arm64/boot/dts/qcom/
